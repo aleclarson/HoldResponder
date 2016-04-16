374f1564422021065063d826f32484c2fb8865ee
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-04-16 14:38:07 -0700

    04/16/2016

--- js/src/Holdable.js
+++ js/src/Holdable.js
@@ -1,8 +1,18 @@
-var Event, Factory, Responder, Timer;
+var Event, Factory, Responder, Timer, assertType, combine, define, emptyFunction, simulateNativeEvent;
+
+assertType = require("type-utils").assertType;
 
 Responder = require("gesture").Responder;
 
+simulateNativeEvent = require("simulateNativeEvent");
+
+emptyFunction = require("emptyFunction");
+
+combine = require("combine");
+
 Factory = require("factory");
+
+define = require("define");
 
 Timer = require("timer");
 
@@ -12,66 +22,165 @@ module.exports = Factory("Holdable", {
   kind: Responder,
   optionTypes: {
     minHoldTime: Number,
-    preventDistance: Number
+    preventDistance: Number,
+    canHold: Function
   },
   optionDefaults: {
-    preventDistance: Infinity
+    preventDistance: Infinity,
+    canHold: emptyFunction.thatReturnsTrue
   },
   customValues: {
     isHolding: {
       get: function() {
-        return this._holding;
+        return this._isHolding;
       }
     }
   },
-  initFrozenValues: function() {
+  initArguments: function(options) {
+    assert(options.shouldCaptureOnMove == null, "'shouldCaptureOnMove' is not supported by Holdable!");
+    return arguments;
+  },
+  initFrozenValues: function(options) {
     return {
-      minHoldTime: options.minHoldTime,
-      preventDistance: options.preventDistance,
+      didHoldReject: Event(),
       didHoldStart: Event(),
-      didHoldEnd: Event()
+      didHoldEnd: Event(),
+      _minHoldTime: options.minHoldTime,
+      _preventDistance: options.preventDistance,
+      _canHold: options.canHold
     };
   },
   initReactiveValues: function() {
     return {
-      _holding: false,
-      _holdTimer: null
+      _mockListener: null,
+      _captureEvent: null,
+      _isCapturing: false,
+      _holdTimer: null,
+      _isHolding: false
     };
   },
+  init: function() {
+    return this._shouldTerminate = (function(_this) {
+      return function() {
+        log.it("_shouldTerminate: { isHolding: " + _this._isHolding + " }");
+        return !_this._isHolding;
+      };
+    })(this);
+  },
   boundMethods: ["_onHoldStart"],
-  _stopHoldTimer: function() {
+  startTimer: function() {
+    if (this._holdTimer) {
+      return;
+    }
+    this._holdTimer = Timer(this._minHoldTime, this._onHoldStart);
+  },
+  stopTimer: function() {
     if (!this._holdTimer) {
       return;
     }
     this._holdTimer.prevent();
-    return this._holdTimer = null;
+    this._holdTimer = null;
+    this._captureEvent = null;
   },
   _onHoldStart: function() {
+    var event;
     this._holdTimer = null;
-    this._holding = true;
-    return this.didHoldStart.emit(this._gesture);
+    event = this._captureEvent;
+    this._captureEvent = null;
+    if (this !== Responder.capturedResponder) {
+      this._simulateTouchMove(event);
+    }
+    if (this === Responder.capturedResponder) {
+      this._isHolding = true;
+      return this.didHoldStart.emit(this._gesture);
+    } else {
+      return this.didHoldReject.emit(this._gesture);
+    }
   },
-  _onPanResponderGrant: function() {
-    this._holdTimer = Timer(this.minHoldTime, this._onHoldStart);
-    return Responder.prototype._onPanResponderGrant.call(this);
+  _simulateTouchMove: function(event) {
+    var i, len, ref, touch;
+    event.timestamp += 1;
+    ref = event.changedTouches;
+    for (i = 0, len = ref.length; i < len; i++) {
+      touch = ref[i];
+      touch.timestamp += 1;
+    }
+    this._isCapturing = true;
+    simulateNativeEvent(event.target, "onTouchMove", event);
+    return this._isCapturing = false;
   },
-  _onPanResponderMove: function() {
-    var distance;
-    if (!this._gesture) {
+  _mockCapturedResponder: function() {
+    var onListen;
+    if (Responder.capturedResponder !== null) {
+      this._mockResponder(Responder.capturedResponder);
       return;
     }
-    distance = Math.sqrt((Math.pow(this._gesture.dx, 2)) + (Math.pow(this._gesture.dy, 2)));
-    if (distance >= this.preventDistance) {
-      return this._onPanResponderTerminate();
-    }
-    return Responder.prototype._onPanResponderMove.call(this);
+    onListen = Responder.didResponderCapture.once((function(_this) {
+      return function(responder) {
+        return _this._mockResponder(responder);
+      };
+    })(this));
+    return setImmediate((function(_this) {
+      return function() {
+        return onListen.stop();
+      };
+    })(this));
   },
-  _onPanResponderEnd: function() {
-    if (this._holding) {
-      this._holding = false;
-      this.didHoldEnd.emit(this._gesture);
+  _mockResponder: function(responder) {
+    if (!(responder && (responder !== this))) {
+      return;
     }
-    return Responder.prototype._onPanResponderEnd.call(this);
+    return this._mockListener = responder.didEnd.once((function(_this) {
+      return function(gesture) {
+        if (_this._isCapturing) {
+          return;
+        }
+        _this._mockListener = null;
+        return _this._interrupt(gesture.finished);
+      };
+    })(this));
+  },
+  __shouldRespondOnStart: function() {
+    if (!Responder.prototype.__shouldRespondOnStart.apply(this, arguments)) {
+      return false;
+    }
+    this.startTimer();
+    return true;
+  },
+  __shouldCaptureOnStart: function(event) {
+    if (!this._canHold(this._gesture)) {
+      return false;
+    }
+    this.startTimer();
+    if (Responder.prototype.__shouldCaptureOnStart.apply(this, arguments)) {
+      return true;
+    }
+    this._captureEvent = combine({}, event.nativeEvent);
+    this._mockCapturedResponder();
+    return false;
+  },
+  __shouldCaptureOnMove: function(event) {
+    if (this._isCapturing) {
+      return true;
+    }
+    if (Responder.prototype.__shouldCaptureOnMove.apply(this, arguments)) {
+      return true;
+    }
+    this._captureEvent = combine({}, event.nativeEvent);
+    return false;
+  },
+  __onTouchEnd: function() {
+    if (this._isHolding) {
+      this._isHolding = false;
+      this.didHoldEnd.emit(this._gesture);
+    } else {
+      this.stopTimer();
+      if (this._mockListener) {
+        this._mockListener.stop();
+        this._mockListener = null;
+      }
+    }
+    return Responder.prototype.__onTouchEnd.apply(this, arguments);
   }
 });
 
--- src/Holdable.coffee
+++ src/Holdable.coffee
@@ -1,7 +1,15 @@
 
+# TODO: Support multi-touch holding.
+
+{ assertType } = require "type-utils"
+
 { Responder } = require "gesture"
 
+simulateNativeEvent = require "simulateNativeEvent"
+emptyFunction = require "emptyFunction"
+combine = require "combine"
 Factory = require "factory"
+define = require "define"
 Timer = require "timer"
 Event = require "event"
 
@@ -12,57 +20,171 @@ module.exports = Factory "Holdable",
   optionTypes:
     minHoldTime: Number
     preventDistance: Number
+    canHold: Function
 
   optionDefaults:
     preventDistance: Infinity
+    canHold: emptyFunction.thatReturnsTrue
 
   customValues:
 
     isHolding: get: ->
-      @_holding
+      @_isHolding
 
-  initFrozenValues: ->
+  initArguments: (options) ->
+    assert not options.shouldCaptureOnMove?, "'shouldCaptureOnMove' is not supported by Holdable!"
+    arguments
 
-    minHoldTime: options.minHoldTime
+  initFrozenValues: (options) ->
 
-    preventDistance: options.preventDistance
+    didHoldReject: Event()
 
     didHoldStart: Event()
 
     didHoldEnd: Event()
 
+    _minHoldTime: options.minHoldTime
+
+    _preventDistance: options.preventDistance
+
+    _canHold: options.canHold
+
   initReactiveValues: ->
 
-    _holding: no
+    _endListener: null
+
+    _captureEvent: null
+
+    _isCapturing: no
 
     _holdTimer: null
+
+    _isHolding: no
+
+  init: ->
+
+    @_shouldTerminate = =>
+      log.it "_shouldTerminate: { isHolding: #{@_isHolding} }"
+      return not @_isHolding
 
   boundMethods: [
     "_onHoldStart"
   ]
 
-  _stopHoldTimer: ->
+  startTimer: ->
+    return if @_holdTimer
+    @_holdTimer = Timer @_minHoldTime, @_onHoldStart
+    return
+
+  stopTimer: ->
     return unless @_holdTimer
     @_holdTimer.prevent()
     @_holdTimer = null
+    @_captureEvent = null
+    return
 
   _onHoldStart: ->
+
     @_holdTimer = null
-    @_holding = yes
-    @didHoldStart.emit @_gesture
 
-  _onPanResponderGrant: ->
-    @_holdTimer = Timer @minHoldTime, @_onHoldStart
-    Responder::_onPanResponderGrant.call this
+    event = @_captureEvent
+    @_captureEvent = null
 
-  _onPanResponderMove: ->
-    return unless @_gesture
-    distance = Math.sqrt (Math.pow @_gesture.dx, 2) + (Math.pow @_gesture.dy, 2)
-    return @_onPanResponderTerminate() if distance >= @preventDistance
-    Responder::_onPanResponderMove.call this
+    if this isnt Responder.capturedResponder
+      @_simulateTouchMove event
 
-  _onPanResponderEnd: ->
-    if @_holding
-      @_holding = no
+    if this is Responder.capturedResponder
+      @_isHolding = yes
+      @didHoldStart.emit @_gesture
+
+    else
+      @didHoldReject.emit @_gesture
+
+  _simulateTouchMove: (event) ->
+
+    event.timestamp += 0.001
+    for touch in event.changedTouches
+      touch.timestamp += 0.001
+
+    @_isCapturing = yes
+    simulateNativeEvent event.target, "onTouchMove", event
+    @_isCapturing = no
+
+  _onResponderCapture: (callback) ->
+
+    if Responder.capturedResponder isnt null
+      callback Responder.capturedResponder
+      return
+
+    onCapture = Responder.didResponderCapture
+      .once callback
+
+    # If no responder captures, stop listening!
+    setImmediate -> onCapture.stop()
+    return
+
+  _onResponderEnd: (responder, callback) ->
+    @_endListener.stop() if @_endListener
+    @_endListener = responder.didEnd.once (gesture) =>
+      @_endListener = null
+      callback gesture
+
+  _trackCapturedResponder: ->
+    @_onResponderCapture (responder) ->
+      return if responder is this
+      @_onResponderEnd responder, (gesture) ->
+        return if @_isCapturing
+        @_interrupt gesture.finished
+
+#
+# Subclass overrides
+#
+
+  __shouldRespondOnStart: ->
+    return no unless Responder::__shouldRespondOnStart.apply this, arguments
+    @startTimer()
+    return yes
+
+  __shouldCaptureOnStart: (event) ->
+    return no unless @_canHold @_gesture
+    @startTimer()
+    return yes if Responder::__shouldCaptureOnStart.apply this, arguments
+    @_captureEvent = combine {}, event.nativeEvent
+    @_trackCapturedResponder()
+    return no
+
+  __shouldCaptureOnMove: (event) ->
+    return yes if @_isCapturing
+    return yes if Responder::__shouldCaptureOnMove.apply this, arguments
+    @_captureEvent = combine {}, event.nativeEvent
+    return no
+
+  # __onTouchMove: ->
+  #
+  #   distance = Math.sqrt (Math.pow @_gesture.dx, 2) + (Math.pow @_gesture.dy, 2)
+  #
+  #   log.moat 1
+  #   log.it @__id + ".onTouchMove()"
+  #   log.it "distance = " + distance
+  #   log.moat 1
+  #
+  #   if (not @_isHolding) and (distance >= @_preventDistance)
+  #     @terminate()
+  #     return
+  #
+  #   Responder::__onTouchMove.apply this, arguments
+
+  __onTouchEnd: ->
+
+    if @_isHolding
+      @_isHolding = no
       @didHoldEnd.emit @_gesture
-    Responder::_onPanResponderEnd.call this
+
+    else
+      @stopTimer()
+
+      if @_endListener
+        @_endListener.stop()
+        @_endListener = null
+
+    Responder::__onTouchEnd.apply this, arguments
